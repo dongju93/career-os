1e3aa7f85a54c56a2d0e0252f707faef98765cfc
dongju93 <spdlqj011@gmail.com>
2026-04-09 02:33:10 +0900

    :wrench: Add openai api config

--- career_os_api/config.py
+++ career_os_api/config.py
@@ -1,12 +1,15 @@
+from typing import ClassVar
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config: SettingsConfigDict = SettingsConfigDict(
+    model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8"
     )
 
     database_url: str
+    openai_api_key: str
     api_v1: str = "v1"
     api_v2: str = "v2"
 
